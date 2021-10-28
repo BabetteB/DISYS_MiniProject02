d21f41523ae9f79a11b08f445a8182722097b774
clyt <clyt@itu.dk>
2021-10-28 18:44:35 +0200

    proto-file changed to reflect pub/sub pattern

--- chat/chatHandler.go
+++ chat/chatHandler.go
@@ -15,11 +15,6 @@ var (
 type Server struct {
 }
 
-/* func (s *Server) SayHello(ctx context.Context, in *Message) (*Message, error) {
-	log.Printf("Receive message body from client: %s", in.Body)
-	return &Message{Body: "Hello From the Server!"}, nil
-} */
-
 func (s *Server) Publish(ctx context.Context, in *ClientMessage) (*StatusMessage, error) {
 	//log.Printf("Receive message body from client: %s", in.Body)
 	response := StatusMessage{ Status: Status_SUCCESS};
